b07a984d94439c3673dd36739eff4d04bfa6b3fc
Ike Maldonado <maldonadoike@gmail.com>
2023-02-12 11:48:13 -0800

    add user card

---
+++ app/Login.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { UserCard } from './userCard';
+
+export default function Login() {
+	// get session fron nextAuth
+	const { data: session } = useSession();
+	// if user exists => show a Sign Out button and thier information
+
+	if (session) {
+		return (
+			<>
+				<button
+					onClick={() => signOut()}
+					type='button'
+					className='btn btn-primary'
+				>
+					Sign Out of Google
+				</button>
+				{/* Pass session info to server component*/}
+
+        <UserCard user={session?.user} />
+			</>
+		);
+	} else {
+		return (
+			<>
+				<button
+					onClick={() => signIn()}
+					type='button'
+					className='btn btn-primary'
+				>
+					Sign In with Google
+				</button>
+			</>
+		);
+	}
+	// if user does not exist => show a Sign In button
+}
---
+++ app/userCard.tsx
@@ -0,0 +1,25 @@
+import { DefaultSession } from 'next-auth';
+import Image from 'next/image';
+export function UserCard({ user }: { user: DefaultSession['user'] }) {
+	console.log(user?.image)
+	return (
+		<div className='card w-96 bg-base-100 shadow-xl'>
+			<figure>
+				<Image
+					src={user?.image}
+					width={200}
+					height={200}
+					alt='profile pic'
+				/>
+			</figure>
+			<div className='card-body'>
+				<h2 className='card-title'>Current Logged In User</h2>
+				<h3>{user?.name}</h3>
+				<h3>{user?.email}</h3>
+				<div className='card-actions justify-end'>
+					<button className='btn btn-primary'>Buy Now</button>
+				</div>
+			</div>
+		</div>
+	);
+}
